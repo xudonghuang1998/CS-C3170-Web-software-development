6b37f6d1dc8788aa74665b8f9b5bfb57574c4156
黄旭东 <72771002+xudonghuang1998@users.noreply.github.com>
2020-12-13 16:16:32 +0200

    Create controller_test.js

---
+++ Testing Web Applications/Full test coverage/routes/controllers/controller_test.js
@@ -0,0 +1,7 @@
+import { superoak } from "https://deno.land/x/superoak@2.3.1/mod.ts";
+import { app } from "../../app.js";
+
+Deno.test("GET '/'", async () => {
+    const testClient = await superoak(app);
+    await testClient.get("/");
+});
