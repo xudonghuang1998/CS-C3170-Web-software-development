09df99c4f63ca1e417030cb2a87a1812a7657960
黄旭东 <72771002+xudonghuang1998@users.noreply.github.com>
2020-12-13 15:19:02 +0200

    Create app.js

---
+++ Structuring Web Applications/Structuring Web Applications/app.js
@@ -0,0 +1,24 @@
+import { Application } from "./deps.js";
+import { router } from "./routes/routes.js";
+import * as middleware from './middlewares/middlewares.js';
+import { viewEngine, engineFactory, adapterFactory } from "./deps.js";
+
+const app = new Application();
+
+const ejsEngine = engineFactory.getEjsEngine();
+const oakAdapter = adapterFactory.getOakAdapter();
+app.use(viewEngine(oakAdapter, ejsEngine, {
+    viewRoot: "./views"
+}));
+
+app.use(middleware.errorMiddleware);
+app.use(middleware.requestTimingMiddleware);
+app.use(middleware.serveStaticFilesMiddleware);
+
+app.use(router.routes());
+
+if (!Deno.env.get('TEST_ENVIRONMENT')) {
+  app.listen({ port: 7777 });
+}
+    
+export default app;
