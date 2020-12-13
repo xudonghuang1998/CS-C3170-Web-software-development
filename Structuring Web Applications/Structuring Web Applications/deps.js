9083a928dc696bf437af840682223957d016848e
黄旭东 <72771002+xudonghuang1998@users.noreply.github.com>
2020-12-13 15:19:53 +0200

    Create deps.js

---
+++ Structuring Web Applications/Structuring Web Applications/deps.js
@@ -0,0 +1,3 @@
+export { Application, Router, send } from "https://deno.land/x/oak@v6.3.2/mod.ts";
+export { viewEngine, engineFactory, adapterFactory } from "https://raw.githubusercontent.com/deligenius/view-engine/master/mod.ts";
+export { Client } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
