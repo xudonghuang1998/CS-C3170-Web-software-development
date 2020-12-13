4bf0bc6cb33a94889cbd830e839478d7bf872ff6
黄旭东 <72771002+xudonghuang1998@users.noreply.github.com>
2020-12-13 06:33:36 +0200

    Create app.js

---
+++ Views and Templates/Starting with templates/app.js
@@ -0,0 +1,12 @@
+import { serve } from "https://deno.land/std@0.65.0/http/server.ts";
+import { renderFile } from 'https://raw.githubusercontent.com/syumai/dejs/master/mod.ts';
+
+const server = serve({ port: 7777 });
+
+const data = {
+    title: 'Hello EJS Templates!'
+};
+
+for await (const request of server) {
+    request.respond({ body: await renderFile('index.ejs', data) });
+}
