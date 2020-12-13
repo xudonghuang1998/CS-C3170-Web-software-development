144be72639e6338ad21280726d63a2d9c74bf557
黄旭东 <72771002+xudonghuang1998@users.noreply.github.com>
2020-12-13 14:57:17 +0200

    Create app.js

---
+++ Introduction to Web Frameworks/Ticketing system 4/app.js
@@ -0,0 +1,70 @@
+import { Application, Router } from "https://deno.land/x/oak@v6.3.2/mod.ts";
+import { viewEngine, engineFactory, adapterFactory } from "https://raw.githubusercontent.com/deligenius/view-engine/master/mod.ts";
+import { Client } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
+
+const client = new Client({
+});
+
+const app = new Application();
+const router = new Router();
+
+const ejsEngine = engineFactory.getEjsEngine();
+const oakAdapter = adapterFactory.getOakAdapter();
+app.use(viewEngine(oakAdapter, ejsEngine));
+
+const executeQuery = async(query, ...args) => {
+  try {
+      await client.connect();
+      return await client.query(query, ...args);
+  } catch (e) {
+      console.log(e);
+  } finally {
+      await client.end();
+  }
+}
+
+const listTickets = async({render}) => {
+  const res = await executeQuery('SELECT * FROM tickets');
+  let rows = [];
+  if (res) {
+    rows = res.rowsOfObjects();
+  }
+
+  const data = {
+    tickets: rows
+  };
+  
+  render('index.ejs', data);
+}
+
+const addTicket = async({request, render}) => {
+  const body = request.body();
+  const params = await body.value;
+  const content = params.get('content');
+
+  await executeQuery('INSERT INTO tickets (content, reported_on) VALUES ($1, NOW());', content);
+  await listTickets({render: render});
+}
+
+const resolveTicket = async({params, render}) => {
+  await executeQuery('UPDATE tickets SET resolved_on = NOW() WHERE id = $1', params.id);
+  await listTickets({render: render});
+}
+
+const deleteTicket = async({params, render}) => {
+  await executeQuery('DELETE FROM tickets WHERE id = $1', params.id);
+  await listTickets({render: render});
+}
+
+router.get('/tickets', listTickets);
+router.post('/tickets', addTicket);
+router.post('/tickets/:id/resolve', resolveTicket);
+router.post('/tickets/:id/delete', deleteTicket);
+
+app.use(router.routes());
+
+if (!Deno.env.get('TEST_ENVIRONMENT')) {
+  app.listen({ port: 7777 });
+}
+
+export default app;
