cb67b3332a652b353e272591e3af7c5d3727ac06
黄旭东 <72771002+xudonghuang1998@users.noreply.github.com>
2020-12-13 14:54:34 +0200

    Create app.js
    CREATE TABLE tickets (
        id SERIAL PRIMARY KEY,
        content TEXT NOT NULL,
        reported_on TIMESTAMP WITH TIME ZONE,
        resolved_on TIMESTAMP WITH TIME ZONE
    );

---
+++ Introduction to Web Frameworks/Ticketing system 1/app.js
@@ -0,0 +1,48 @@
+import { Application, Router } from "https://deno.land/x/oak@v6.3.2/mod.ts";
+import { viewEngine, engineFactory, adapterFactory } from "https://raw.githubusercontent.com/deligenius/view-engine/master/mod.ts";
+import { Client } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
+
+const client = new Client({
+});
+
+const app = new Application();
+const router = new Router();
+
+const ejsEngine = engineFactory.getEjsEngine();
+const oakAdapter = adapterFactory.getOakAdapter();
+app.use(viewEngine(oakAdapter, ejsEngine));
+
+const executeQuery = async(query, ...args) => {
+  try {
+      await client.connect();
+      return await client.query(query, ...args);
+  } catch (e) {
+      console.log(e);
+  } finally {
+      await client.end();
+  }
+}
+
+const listTickets = async({render}) => {
+  const res = await executeQuery('SELECT * FROM tickets');
+  let rows = [];
+  if (res) {
+    rows = res.rowsOfObjects();
+  }
+
+  const data = {
+    tickets: rows
+  };
+  
+  render('index.ejs', data);
+}
+
+router.get('/tickets', listTickets);
+
+app.use(router.routes());
+
+if (!Deno.env.get('TEST_ENVIRONMENT')) {
+  app.listen({ port: 7777 });
+}
+
+export default app;
