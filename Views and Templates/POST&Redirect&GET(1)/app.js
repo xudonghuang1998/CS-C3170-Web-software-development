3729d1fe518f3f08cfa5110bffe1ac89903b1922
黄旭东 <72771002+xudonghuang1998@users.noreply.github.com>
2020-12-13 06:48:06 +0200

    Create app.js

---
+++ Views and Templates/POST&Redirect&GET(1)/app.js
@@ -0,0 +1,30 @@
+import { decode } from "https://deno.land/std@0.65.0/encoding/utf8.ts";
+import { serve } from "https://deno.land/std@0.65.0/http/server.ts";
+import { renderFile } from 'https://raw.githubusercontent.com/syumai/dejs/master/mod.ts';
+
+const server = serve({ port: 7777 });
+
+const processBody = async(request) => {
+    const bodyArr = await Deno.readAll(request.body);
+    const body = decode(bodyArr);
+    const params = new URLSearchParams(body);
+    data.content = params.get('content');
+}
+
+const data = {
+    content: 'Nothing'
+};
+
+for await (const request of server) {
+    if (request.method === 'POST') {
+        await processBody(request);
+        request.respond({
+            status: 303,
+            headers: new Headers({
+                'Location': request.url
+            })
+        });
+    } else {
+        request.respond({ body: await renderFile('index.ejs', data) });
+    }
+}
